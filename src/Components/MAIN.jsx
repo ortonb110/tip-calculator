9c38fb578b636462fe9e6b080f42f29909561834
Bright Orton <ortonb110@gmail.com>
2023-02-06 00:28:04 +0000

    85% done

--- src/Components/MAIN.jsx
+++ src/Components/MAIN.jsx
@@ -1,36 +1,63 @@
 import peopleIcon from "../Assets/images/icon-person.svg";
 import logo from "../Assets/images/logo.svg";
 import currency from "../Assets/images/icon-dollar.svg";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 
 const MAIN = () => {
-  const [amount, setAmount] = useState(0);
-  const [percentage, setPercentage] = useState(0);
-  const [numberOfpeople, setNumberOfPeople] = useState(0);
-  const [tipAmount, setTipAmount] = useState(0);
+  const [amount, setAmount] = useState("");
+  const [percentage, setPercentage] = useState("");
+  const [numberOfPeople, setNumberOfPeople] = useState("");
+  const [tipAmount, setTipAmount] = useState("0.00");
   const [totalAmount, setTotalAmount] = useState(0);
+  const [people, setPeople] = useState(true);
+  const [amountBool, setAmountBool] = useState(true);
 
   const getAmountValue = (event) => {
     setAmount(event.target.value);
-    
+    setAmountBool(true);
   };
 
   const getPercentage = (event) => {
+    amount <= 1 ? setAmountBool(false) : setAmountBool(true);
+    numberOfPeople <= 1 ? setPeople(false) : setPeople(true);
     setPercentage(event.target.value);
+  };
+
+  const getNumberOfPeople = (event) => {
+    setNumberOfPeople(event.target.value);
+    setPeople(true);
+  };
+
+  function computeResult() {
+    if (people === true && amountBool === true) {
+      return (amount * (percentage / 100)) / numberOfPeople;
+    } else {
+      return "0.00";
+    }
+  }
+  function computeTotalAmount() {
+    if (people === true && amountBool === true) {
+      return ((amount * (percentage / 100)) / numberOfPeople) * numberOfPeople;
+    } else {
+      return "0.00";
+    }
   }
 
-  
+  const resetBtn = () => {
+    setAmount("");
+    setNumberOfPeople("");
+    setPercentage("");
+    setTotalAmount("0.00");
+    setTipAmount("0.00");
+  };
 
-
-  useEffect(()=>
-    {
-        console.log(amount);
-        console.log(percentage)
-    },[amount,percentage]
-  )
+  useEffect(() => {
+    setTipAmount(computeResult());
+    setTotalAmount(computeTotalAmount);
+  }, [tipAmount, numberOfPeople, amount, percentage, totalAmount]);
 
   return (
-    <>
+    <Fragment>
       <header className="text-center">
         <a href="../App.js" id="logo">
           <img
@@ -47,10 +74,17 @@ const MAIN = () => {
             <div className="flex flex-col mb-[4rem]">
               <label
                 htmlFor="amount"
-                className="text-[1.6rem] text-darkGrayishCyan mb-[0.6rem]"
+                className=" mb-[0.6rem] text-[1.6rem] flex justify-between"
                 aria-controls="amount"
               >
-                Bill
+                <span className=" text-darkGrayishCyan">Bill</span>
+                <span
+                  className={`text-red-400 invisible transition ${
+                    amountBool ? "" : "!visible"
+                  }`}
+                >
+                  Can't be zero
+                </span>
               </label>
               <div className="relative">
                 <img src={currency} alt="" className="inputicon" />
@@ -58,8 +92,11 @@ const MAIN = () => {
                   type="number"
                   id="amount"
                   placeholder="0"
+                  value={amount}
                   onChange={getAmountValue}
-                  className="input focus:border-primaryColor focus:border-2 transition"
+                  className={`input focus:border-primaryColor focus:border-2 transition ${
+                    amountBool ? "" : "border-red-400 border-2"
+                  }`}
                 />
               </div>
             </div>
@@ -69,36 +106,74 @@ const MAIN = () => {
               </h3>
               <ul className="grid md:grid-cols-3 gap-5 mt-[1.6rem] grid-cols-2">
                 <li className="span-col-1">
-                  <button onClick={getPercentage} value="5" className="btn  btn-hover-active">5%</button>
+                  <button
+                    onClick={getPercentage}
+                    value="5"
+                    className="btn  btn-hover-active "
+                  >
+                    5%
+                  </button>
                 </li>
                 <li className="span-col-1">
-                  <button onClick={getPercentage} value="10" className="btn  btn-hover-active">10%</button>
+                  <button
+                    onClick={getPercentage}
+                    value="10"
+                    className="btn  btn-hover-active"
+                  >
+                    10%
+                  </button>
                 </li>
                 <li className="span-col-1">
-                  <button onClick={getPercentage} value="15" className="btn  btn-hover-active">15%</button>
+                  <button
+                    onClick={getPercentage}
+                    value="15"
+                    className="btn  btn-hover-active"
+                  >
+                    15%
+                  </button>
                 </li>
                 <li className="span-col-1">
-                  <button onClick={getPercentage} value="25" className="btn btn-hover-active ">25%</button>
+                  <button
+                    onClick={getPercentage}
+                    value="25"
+                    className="btn btn-hover-active "
+                  >
+                    25%
+                  </button>
                 </li>
                 <li className="span-col-1">
-                  <button onClick={getPercentage} value="50" className="btn  btn-hover-active">50%</button>
+                  <button
+                    onClick={getPercentage}
+                    value="50"
+                    className="btn  btn-hover-active"
+                  >
+                    50%
+                  </button>
                 </li>
                 <li className="span-col-1">
                   <input
                     type="number"
                     placeholder="Custom"
+                    value={percentage}
                     onChange={getPercentage}
                     className="input placeholder-darkGrayishCyan w-[11.7rem] rounded-md h-[4.8rem] focus:border-primaryColor focus:border-2 transition"
-                  />{" "}
+                  />
                 </li>
               </ul>
             </div>
             <div className="flex flex-col">
               <label
                 htmlFor="people"
-                className="text-[1.6rem] text-darkGrayishCyan mb-[0.6rem]"
+                className="text-[1.6rem]  mb-[0.6rem] flex justify-between"
               >
-                Number of People
+                <span className="text-darkGrayishCyan">Number of People</span>
+                <span
+                  className={`text-red-400 invisible transition ${
+                    people ? "" : "!visible"
+                  }`}
+                >
+                  Can't be zero
+                </span>
               </label>
 
               <div className="relative">
@@ -107,7 +182,11 @@ const MAIN = () => {
                   type="number"
                   id="people"
                   placeholder="0"
-                  className="input focus:border-primaryColor focus:border-2 transition"
+                  value={numberOfPeople}
+                  onChange={getNumberOfPeople}
+                  className={`input focus:border-primaryColor focus:border-2 transition ${
+                    people ? "" : "border-red-400 border-2"
+                  }`}
                 />
               </div>
             </div>
@@ -120,7 +199,7 @@ const MAIN = () => {
               </p>
               <p className="text-primaryColor text-[4.8rem]">
                 <span>$</span>
-                <span>0.00</span>
+                <span>{numberOfPeople ? tipAmount : "0.00"}</span>
               </p>
             </div>
             <div className="text-white flex justify-between items-center mt-[4.1rem]">
@@ -130,16 +209,19 @@ const MAIN = () => {
               </p>
               <p className="text-primaryColor text-[4.8rem]">
                 <span>$</span>
-                <span>0.00</span>
+                <span>{numberOfPeople ? totalAmount : "0.00"}</span>
               </p>
             </div>
-            <button className="btn mt-[10rem] text-[2rem] bg-primaryColor uppercase w-full text-veryDarkCyan  transition hover:bg-veryLightGrayishCyan active:translate-y-1">
+            <button
+              onClick={resetBtn}
+              className="btn mt-[10rem] text-[2rem] bg-primaryColor uppercase w-full text-veryDarkCyan  transition hover:bg-veryLightGrayishCyan active:translate-y-1"
+            >
               reset
             </button>
           </section>
         </div>
       </main>
-    </>
+    </Fragment>
   );
 };
 
